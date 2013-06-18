bdeb85f1ed0a2e3fed172272bd6592cfb744cdf1
Jeremy Parker <jawparker@gmail.com>
2013-06-18 17:18:50 +1000

    allow XR code to pull methods out of closures

--- src/types/closure.c
+++ src/types/closure.c
@@ -27,7 +27,18 @@ XR closure_literal(XR cl, XR self)
     return xr_str("(closure)");
 }
 
+XR closure_get_method(XR cl, XR self)
+{
+    (void) cl;
+
+    if (((struct XRClosure*)self)->native == 1)
+        assert(0 && "Trying to get method on native closure");
+
+    return xrClosureAt(self, 0);
+}
+
 void xr_closure_methods()
 {
     def_method(closure_vt, s_literal, closure_literal);
+    qdef_method(closure_vt, "get_m", closure_get_method);
 }

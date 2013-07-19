b6b3eaab41a7983c50cb12f26c4e4125ff8a376a
Jeremy Parker <jawparker@gmail.com>
2013-07-19 18:20:52 +1000

    just messing around with tests for no good reason

--- xr_tests/run_test.sh
+++ xr_tests/run_test.sh
@@ -2,7 +2,7 @@
 file=$1
 
 out=`cat .prelude.xr - .finale.xr < $file | ../parse_dump 2> /dev/null`
-expect=`cat $file | sed '/\#-/!d; s/.*\#- //'`
+expect=`cat $file | sed '/\#-/!d; s/.*\#-//'`
 if [ "$out" != "$expect" ]; then
     echo "$file: expected <$expect>, but got <$out>";
 else

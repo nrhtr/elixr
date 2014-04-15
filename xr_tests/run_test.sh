3b608df2d769404562353387b41d40f0efe42c68
Jeremy Parker <jawparker@gmail.com>
2014-04-15 13:48:05 +1000

    strip shebangs before running tests

--- xr_tests/run_test.sh
+++ xr_tests/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 file=$1
 
-out=`cat .prelude.xr - .finale.xr < $file | ../parse_dump 2> stderr.log`
-expect=`cat $file | sed '/\#/!d; s/.*\#//'`
+out=`cat .prelude.xr - .finale.xr < $file | ../parse_dump 2>> stderr.log`
+expect=`cat $file | sed 's/#!.*//; /\#/!d; s/.*\#//'`
 if [ "$out" != "$expect" ]; then
     echo "$file: expected <$expect>, but got <$out>";
 else

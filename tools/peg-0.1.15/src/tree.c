a4c77104cc69c44fe807ecf7449ca7f2fe465068
Jeremy Parker <jawparker@gmail.com>
2015-08-06 19:50:28 +1000

    Update peg/leg version.

--- tools/peg-0.1.15/src/tree.c
+++ tools/peg-0.1.15/src/tree.c
@@ -13,13 +13,18 @@
  * 
  * THE SOFTWARE IS PROVIDED 'AS IS'.  USE ENTIRELY AT YOUR OWN RISK.
  * 
- * Last edited: 2007-05-15 10:32:09 by piumarta on emilia
+ * Last edited: 2013-07-20 12:47:35 by piumarta on margaux1
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+
+#ifdef WIN32
+# undef inline
+# define inline __inline
+#endif
 
 #include "tree.h"
 
@@ -150,7 +155,7 @@ Node *makeAction(char *text)
     char *ptr;
     for (ptr= node->action.text;  *ptr;  ++ptr)
       if ('$' == ptr[0] && '$' == ptr[1])
-	ptr[1]= ptr[0]= 'y';
+	ptr[1]= ptr[0]= '_';
   }
   return node;
 }
@@ -159,6 +164,14 @@ Node *makePredicate(char *text)
 {
   Node *node= newNode(Predicate);
   node->predicate.text= strdup(text);
+  return node;
+}
+
+Node *makeError(Node *e, char *text)
+{
+  Node *node= newNode(Error);
+  node->error.element= e;
+  node->error.text= strdup(text);
   return node;
 }
 
@@ -297,6 +310,7 @@ static void Node_fprint(FILE *stream, Node *node)
   switch (node->type)
     {
     case Rule:		fprintf(stream, " %s", node->rule.name);				break;
+    case Variable:	fprintf(stream, " %s:", node->variable.name);				break;
     case Name:		fprintf(stream, " %s", node->name.rule->rule.name);			break;
     case Dot:		fprintf(stream, " .");							break;
     case Character:	fprintf(stream, " '%s'", node->character.value);			break;
